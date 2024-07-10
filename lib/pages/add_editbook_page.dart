13a6d0e7f5faa7a93948b8980d4a36fa8b16a435
Franki99 <ntaganiradivanni@gmail.com>
2024-07-10 17:13:33 +0300

    Customized the add & edit book page

--- lib/pages/add_editbook_page.dart
+++ lib/pages/add_editbook_page.dart
@@ -1,7 +1,9 @@
+import 'package:book_lab/theme/theme_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:book_lab/models/book.dart';
 import 'package:book_lab/providers/book_provider.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
 class AddEditBookPage extends StatefulWidget {
   final Book? book;
@@ -58,86 +60,168 @@ class _AddEditBookPageState extends State<AddEditBookPage> {
 
   @override
   Widget build(BuildContext context) {
+    final themeProvider = Provider.of<ThemeProvider>(context);
+    final isDarkMode = themeProvider.themeData.brightness == Brightness.dark;
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.book == null ? 'Add Book' : 'Edit Book'),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.save),
-            onPressed: _saveForm,
-          ),
-        ],
       ),
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.all(16.0),
-          child: Form(
-            key: _formKey, // Ensure _formKey is correctly assigned here
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                TextFormField(
-                  controller: _titleController,
-                  decoration: InputDecoration(labelText: 'Title'),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter a title';
-                    }
-                    return null;
-                  },
-                ),
-                SizedBox(height: 16),
-                TextFormField(
-                  controller: _authorController,
-                  decoration: InputDecoration(labelText: 'Author'),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter an author';
-                    }
-                    return null;
-                  },
-                ),
-                SizedBox(height: 16),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          child: Center(
+            child: Container(
+              width: 400,
+              padding: const EdgeInsets.all(16.0),
+              margin: const EdgeInsets.only(top: 50.0), // Move the box lower
+              decoration: BoxDecoration(
+                color: isDarkMode
+                    ? Colours.midnightBlue
+                    : Colors.black, // Set background color
+                borderRadius: BorderRadius.circular(16),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black12,
+                    blurRadius: 8,
+                  ),
+                ],
+              ),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
                   children: [
-                    Text('Rating:'),
-                    DropdownButton<int>(
-                      value: _rating,
-                      onChanged: (newValue) {
+                    TextFormField(
+                      controller: _titleController,
+                      style: TextStyle(
+                          color:
+                              isDarkMode ? Colors.grey[100] : Colors.grey[100]),
+                      decoration: InputDecoration(
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                                color:
+                                    isDarkMode ? Colors.white : Colors.white),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.grey.shade600),
+                          ),
+                          labelText: 'Title',
+                          labelStyle: TextStyle(
+                            color: isDarkMode ? Colors.white : Colors.white,
+                          )),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter a title';
+                        }
+                        return null;
+                      },
+                    ),
+                    SizedBox(height: 16),
+                    TextFormField(
+                      controller: _authorController,
+                      style: TextStyle(
+                          color:
+                              isDarkMode ? Colors.grey[100] : Colors.grey[100]),
+                      decoration: InputDecoration(
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                                color:
+                                    isDarkMode ? Colors.white : Colors.white),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.grey.shade600),
+                          ),
+                          labelText: 'Author',
+                          labelStyle: TextStyle(
+                            color: isDarkMode ? Colors.white : Colors.white,
+                          )),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter an author';
+                        }
+                        return null;
+                      },
+                    ),
+                    SizedBox(height: 16),
+                    Text(
+                      'Rating:',
+                      style: TextStyle(
+                          color: isDarkMode ? Colors.white : Colors.white,
+                          fontSize: 18),
+                    ),
+                    RatingBar.builder(
+                      initialRating: _rating.toDouble(),
+                      minRating: 0,
+                      direction: Axis.horizontal,
+                      allowHalfRating: true,
+                      itemCount: 5,
+                      itemPadding: EdgeInsets.symmetric(horizontal: 4.0),
+                      itemBuilder: (context, _) => Icon(
+                        Icons.star,
+                        color: Colors.amber,
+                      ),
+                      unratedColor:
+                          isDarkMode ? Colors.white54 : Colors.grey[200],
+                      onRatingUpdate: (rating) {
                         setState(() {
-                          _rating = newValue!;
+                          _rating = rating.toInt();
                         });
                       },
-                      items: List.generate(
-                          6,
-                          (index) => DropdownMenuItem(
-                                child: Text(index.toString()),
-                                value: index,
-                              )),
+                    ),
+                    SizedBox(height: 13),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'Read:',
+                          style: TextStyle(
+                              color: isDarkMode ? Colors.white : Colors.white,
+                              fontSize: 18),
+                        ),
+                        Switch(
+                          value: _isRead,
+                          onChanged: (value) {
+                            setState(() {
+                              _isRead = value;
+                            });
+                          },
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 32),
+                    Center(
+                      child: SizedBox(
+                        width: 150, // Adjust the width as needed
+                        child: ElevatedButton(
+                          onPressed: _saveForm,
+                          style: ElevatedButton.styleFrom(
+                            primary: isDarkMode
+                                ? Colours.ggreen
+                                : Colors.red, // Set the button color to red
+                          ),
+                          child: Text(
+                            'Save',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16),
+                          ),
+                        ),
+                      ),
                     ),
                   ],
                 ),
-                SizedBox(height: 16),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text('Read:'),
-                    Switch(
-                      value: _isRead,
-                      onChanged: (value) {
-                        setState(() {
-                          _isRead = value;
-                        });
-                      },
-                    ),
-                  ],
-                ),
-              ],
+              ),
             ),
           ),
         ),
       ),
     );
   }
+}
+
+class Colours {
+  static const aquamarine = Color(0xFF7FFFD4);
+  static const midnightBlue = Color(0xFF0D0D21);
+  static const ggreen = Color(0xFF3247E5);
 }
